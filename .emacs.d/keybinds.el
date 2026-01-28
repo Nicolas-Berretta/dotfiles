43d44b9d73a6fd0bccb57f11d8ea44bbfc0ab6b3
nicolasberretta <neberretta@mail.austral.edu.ar>
2026-01-28 18:16:06 -0300

    small fix

--- .emacs.d/keybinds.el
+++ .emacs.d/keybinds.el
@@ -1,3 +1,3 @@
 ;; Keybindings for my emacs config jijijija
 
-(global-set-key (kbd "s-/") 'comment-region)(global-set-key (kbd "s-/") 'comment-region)(global-set-key (kbd "s-/") 'comment-region)
+(global-set-key (kbd "s-/") 'comment-region)
